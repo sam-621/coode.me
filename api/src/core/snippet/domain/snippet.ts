64673be6a0f538bbce748cdc633717e90900db9c
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-05-28 01:48:25 -0700

    feat(shared):  add primitives

--- api/src/core/snippet/domain/snippet.ts
+++ api/src/core/snippet/domain/snippet.ts
@@ -1,12 +1,20 @@
+import { Primitives } from '@/core/shared/domain';
+import { SnippetDescription } from './components';
+
 export class Snippet {
   private constructor(
     readonly id: string,
     readonly code: string,
     readonly language: string,
-    readonly description: string,
+    readonly description: SnippetDescription,
   ) {}
 
-  public static create({ id, code, language, description }: Snippet) {
-    return new Snippet(id, code, language, description);
+  public static create({
+    id,
+    code,
+    language,
+    description,
+  }: Primitives<Snippet>) {
+    return new Snippet(id, code, language, new SnippetDescription(description));
   }
 }
