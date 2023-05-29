21bcc7088db39c887b2b54ce151a2c6b55b6c3ad
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-05-28 23:26:08 -0700

    feat(snippet):  update SnippetRepository

--- api/src/core/snippet/domain/snippet-repository.ts
+++ api/src/core/snippet/domain/snippet-repository.ts
@@ -5,4 +5,8 @@ import { PrimitiveSnippet } from './snippet';
 export interface SnippetRepository {
   findUnique(id: Uuid): Promise<PrimitiveSnippet | null>;
   findMany(): Promise<PrimitiveSnippet[]>;
+  create(snippet: PrimitiveSnippet): Promise<PrimitiveSnippet>;
+  update(snippet: UpdateSnippetInput): Promise<PrimitiveSnippet>;
 }
+
+export type UpdateSnippetInput = Omit<PrimitiveSnippet, 'userId'>;
--- api/src/core/snippet/infrastructure/persistance/snippet-postgres-repository.ts
+++ api/src/core/snippet/infrastructure/persistance/snippet-postgres-repository.ts
@@ -3,19 +3,25 @@ import { Injectable } from '@nestjs/common';
 import { Uuid } from '@/core/shared/domain';
 import { PrismaService } from '@/core/shared/infrastructure';
 
-import { PrimitiveSnippet, SnippetRepository } from '../../domain';
+import { PrimitiveSnippet, SnippetRepository, UpdateSnippetInput } from '../../domain';
 
 @Injectable()
 export class SnippetPostgresRepository implements SnippetRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
   async findUnique(id: Uuid): Promise<PrimitiveSnippet | null> {
-    return this.prismaService.snippet.findUnique({
-      where: { id: id.value }
-    });
+    return this.prismaService.snippet.findUnique({ where: { id: id.value } });
   }
 
   findMany(): Promise<PrimitiveSnippet[]> {
     return this.prismaService.snippet.findMany();
+  }
+
+  create(snippet: PrimitiveSnippet): Promise<PrimitiveSnippet> {
+    return this.prismaService.snippet.create({ data: snippet });
+  }
+
+  update(snippet: UpdateSnippetInput): Promise<PrimitiveSnippet> {
+    return this.prismaService.snippet.update({ data: snippet, where: { id: snippet.id } });
   }
 }
