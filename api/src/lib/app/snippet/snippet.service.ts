a721948550267634f2b84970153d9a504142359c
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-05-30 10:20:45 -0700

    feat(snippet):  implement findMany and findUnique in snippet service class

--- api/src/lib/app/snippet/snippet.service.ts
+++ api/src/lib/app/snippet/snippet.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { SnippetFinder } from '@/core/snippet/application';
+import { PrimitiveSnippet } from '@/core/snippet/domain';
+import { SnippetPostgresRepository } from '@/core/snippet/infrastructure';
+
 @Injectable()
-export class SnippetService {}
+export class SnippetService {
+  constructor(private snippetPostgresRepository: SnippetPostgresRepository) {}
+
+  findMany(): Promise<PrimitiveSnippet[]> {
+    const snippetFinder = new SnippetFinder(this.snippetPostgresRepository);
+
+    return snippetFinder.findMany();
+  }
+
+  findUnique(id: string): Promise<PrimitiveSnippet | null> {
+    const snippetFinder = new SnippetFinder(this.snippetPostgresRepository);
+
+    return snippetFinder.findUnique(id);
+  }
+}
