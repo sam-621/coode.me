327f5430a31edb6e800b95684583773e3b5a4ea6
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-02 14:08:36 -0700

    feat(snippet):  add UpdateSnippetDto

--- api/src/lib/app/snippet/dto/snippet-modifier.dto.ts
+++ api/src/lib/app/snippet/dto/snippet-modifier.dto.ts
@@ -1,11 +1,26 @@
 import { IsInt, IsString, IsUUID, Length } from 'class-validator';
 
-import { SnippetModifierCreateInput } from '@/core/snippet/application';
+import { SnippetModifierCreateInput, SnippetModifierUpdateInput } from '@/core/snippet/application';
 import { SnippetDescription } from '@/core/snippet/domain';
 
 export class CreateSnippetDto implements SnippetModifierCreateInput {
   @IsUUID('4')
   userId: string;
+
+  @IsString()
+  @Length(0, SnippetDescription.DESCRIPTION_MAX_LENGTH)
+  description: string;
+
+  @IsString()
+  code: string;
+
+  @IsInt()
+  language: number;
+}
+
+export class UpdateSnippetDto implements SnippetModifierUpdateInput {
+  @IsUUID('4')
+  id: string;
 
   @IsString()
   @Length(0, SnippetDescription.DESCRIPTION_MAX_LENGTH)
