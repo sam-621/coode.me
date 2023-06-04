a4212bfdfc8b74b09f778266b99407fcdebf8049
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-03 17:40:22 -0700

    chore(scripts):  add is-db-up script to verify is db is up before running tests

---
+++ api/scripts/is-db-up.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+async function isDbUp() {
+  const prisma = new PrismaClient({ errorFormat: 'pretty' });
+
+  try {
+    await prisma.$connect();
+    console.log('✨ Test DB is connected!');
+  } catch (error) {
+    setTimeout(async () => {
+      await prisma.$connect();
+      console.log('✨ Test DB is connected!');
+    }, 3000);
+  }
+}
+
+isDbUp();
