b621b36896a9371660b5bddb5f1ee87723508e27
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-04 13:31:41 -0700

    test(snippet):  update Snippet finder e2e

--- api/tests/e2e/snippet/snippet-finder.e2e-spec.ts
+++ api/tests/e2e/snippet/snippet-finder.e2e-spec.ts
@@ -11,7 +11,7 @@ import { Snippet } from '@/core/snippet/domain';
 import { TestDbHandler } from '@/mock/db/db-handlers';
 import { SnippetFactory, UserFactory } from '@/mock/factory';
 
-describe('Snippet finder', () => {
+describe('Snippet finder e2e', () => {
   let app: INestApplication;
 
   const prisma = new PrismaClient();
@@ -36,37 +36,58 @@ describe('Snippet finder', () => {
     await app.close();
   });
 
-  it(`/GET snippet/all`, async () => {
-    const userFactory = new UserFactory(prisma);
-    const snippetFactory = new SnippetFactory(prisma);
+  describe('/GET snippet/all', () => {
+    it('Should return a list of snippets', async () => {
+      const userFactory = new UserFactory(prisma);
+      const snippetFactory = new SnippetFactory(prisma);
 
-    const { id } = await userFactory.create();
-    await snippetFactory.create(new Uuid(id));
-    await snippetFactory.create(new Uuid(id));
+      const { id } = await userFactory.create();
+      await snippetFactory.create(new Uuid(id));
+      await snippetFactory.create(new Uuid(id));
 
-    const snippetsInDb = await prisma.snippet.count();
+      const snippetsInDb = await prisma.snippet.count();
 
-    const res = await request(app.getHttpServer()).get('/snippet/all');
-    const body: HttpResponse<Primitive<Snippet>[]> = res.body;
+      const res = await request(app.getHttpServer()).get('/snippet/all');
+      const body: HttpResponse<Primitive<Snippet>[]> = res.body;
 
-    expect(res.status).toBe(HttpStatus.OK);
-    expect(body.data.length).toBe(snippetsInDb);
+      expect(res.status).toBe(HttpStatus.OK);
+      expect(body.data.length).toBe(snippetsInDb);
+    });
   });
 
-  it('/GET snippet/:id', async () => {
-    const userFactory = new UserFactory(prisma);
-    const snippetFactory = new SnippetFactory(prisma);
+  describe('/GET snippet/:id', () => {
+    it('Should return the correct snippet with the given id', async () => {
+      const userFactory = new UserFactory(prisma);
+      const snippetFactory = new SnippetFactory(prisma);
 
-    const { id } = await userFactory.create();
-    await snippetFactory.create(new Uuid(id));
-    await snippetFactory.create(new Uuid(id));
+      const { id } = await userFactory.create();
+      await snippetFactory.create(new Uuid(id));
+      await snippetFactory.create(new Uuid(id));
 
-    const snippetToQuery = await prisma.snippet.findFirst();
+      const snippetToQuery = await prisma.snippet.findFirst();
 
-    const res = await request(app.getHttpServer()).get(`/snippet/${snippetToQuery?.id}`);
-    const body: HttpResponse<Primitive<Snippet>> = res.body;
+      const res = await request(app.getHttpServer()).get(`/snippet/${snippetToQuery?.id}`);
+      const body: HttpResponse<Primitive<Snippet>> = res.body;
 
-    expect(res.status).toBe(HttpStatus.OK);
-    expect(body.data.id).toBe(snippetToQuery?.id);
+      expect(res.status).toBe(HttpStatus.OK);
+      expect(body.data.id).toBe(snippetToQuery?.id);
+    });
+
+    it('Should return a null snippet when providing an id that does not exist', async () => {
+      const userFactory = new UserFactory(prisma);
+      const snippetFactory = new SnippetFactory(prisma);
+
+      const { id } = await userFactory.create();
+      await snippetFactory.create(new Uuid(id));
+      await snippetFactory.create(new Uuid(id));
+
+      const randomId = Uuid.create();
+
+      const res = await request(app.getHttpServer()).get(`/snippet/${randomId}`);
+      const body: HttpResponse<Primitive<Snippet>> = res.body;
+
+      expect(res.status).toBe(HttpStatus.OK);
+      expect(body.data).toBe(null);
+    });
   });
 });
