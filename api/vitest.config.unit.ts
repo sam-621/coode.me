dffa7acb86a2c809d49ba91a44f78363ccbbf43b
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-05 11:04:37 -0700

    test(config):  add docker compose file for db integration

--- api/vitest.config.unit.ts
+++ api/vitest.config.unit.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['tests/**/*.test.ts']
+    include: ['./tests/unit/**/*.test.ts']
   },
   resolve: {
     alias: {
