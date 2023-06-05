166ed3230298e1ee3f04e2da142fee9314f21c05
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-05 15:11:09 -0700

    chore(scripts):  update scripts of the app

--- api/scripts/run-e2e.sh
+++ api/scripts/run-e2e.sh
@@ -2,14 +2,14 @@
 # src/run-integration.sh
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-source $DIR/setenv.sh
+source $DIR/load-env.sh
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
 if [ "$#" -eq  "0" ]
   then
-    vitest -c ./vitest.config.e2e.ts
+    yarn test -c ./vitest.config.e2e.ts
 else
-    vitest -c ./vitest.config.e2e.ts --ui
+    yarn test -c ./vitest.config.e2e.ts -w
 fi
