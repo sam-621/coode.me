6fdedcb86a365095518d38ae2b4165fe8a5a9493
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-17 13:44:36 -0700

    feat(projects):  add project module

--- api/src/lib/app/app.module.ts
+++ api/src/lib/app/app.module.ts
@@ -4,9 +4,10 @@ import { ConfigModule } from '@/common/config';
 import { LoggerModule } from '@/common/errors';
 import { PrismaModule } from '@/core/shared/infrastructure';
 
-import { SnippetModule } from './snippet/snippet.module';
+import { ProjectModule } from './project';
+import { SnippetModule } from './snippet';
 
 @Module({
-  imports: [LoggerModule, ConfigModule, PrismaModule, SnippetModule]
+  imports: [LoggerModule, ConfigModule, PrismaModule, SnippetModule, ProjectModule]
 })
 export class AppModule {}
