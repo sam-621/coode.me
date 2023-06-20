411f1667a057935bf06ea20cb9679ffe992af581
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-19 23:56:44 -0700

    chore(web):  add eslint and pretrtier

---
+++ web/src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Text } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hola</h1>
+      {/* <Button>hola</Button> */}
+      <Text>Text</Text>
+    </div>
+  );
+}
