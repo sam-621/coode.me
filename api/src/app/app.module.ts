bd0892f801c4fb8c4bd538f88665fdcd03efa640
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-24 19:21:42 -0700

    feat(app):  add TopicModule

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -6,8 +6,9 @@ import { LoggerModule } from '@/common/errors';
 import { PrismaModule } from './shared/persistance';
 import { ProjectModule } from './project';
 import { SnippetModule } from './snippet';
+import { TopicModule } from './topic';
 
 @Module({
-  imports: [LoggerModule, ConfigModule, PrismaModule, SnippetModule, ProjectModule]
+  imports: [LoggerModule, ConfigModule, PrismaModule, SnippetModule, ProjectModule, TopicModule]
 })
 export class AppModule {}
