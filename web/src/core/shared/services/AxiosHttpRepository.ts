d9917c58850a529e09be80f25e5f452cb57cbd94
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-25 00:11:57 -0700

    feat(shared):  add AxiosHttpRequest

---
+++ web/src/core/shared/services/AxiosHttpRepository.ts
@@ -0,0 +1,90 @@
+import axios, { AxiosResponse } from 'axios';
+
+import { ConfigRequest, Headers, HttpRepository } from '../http-repository';
+
+const UNEXPECTED_ERROR_OCCURRED = 'An unexpected error occurred';
+
+export class AxiosHttpRequest extends HttpRepository {
+  protected apiDomain: string;
+
+  protected endpoint: string;
+
+  protected headers: Headers;
+
+  constructor() {
+    super();
+    this.endpoint = '';
+    this.apiDomain = 'domain';
+    this.headers = {
+      'Content-Type': 'application/json'
+    };
+  }
+
+  protected configRequest(config: ConfigRequest): void {
+    this.endpoint = config.endpoint;
+    this.apiDomain = config.apiDomain || this.apiDomain;
+    this.headers = {
+      ...this.headers,
+      ...config.headers
+    };
+  }
+
+  protected useToken(token: string): void {
+    this.headers = {
+      ...this.headers,
+      authorization: token
+    };
+  }
+
+  protected async get<R>(): Promise<AxiosResponse<R>> {
+    try {
+      return await axios.get(this.urlBuilder(), { headers: this.headers });
+    } catch (error) {
+      if (!axios.isAxiosError(error)) {
+        throw new Error(UNEXPECTED_ERROR_OCCURRED);
+      }
+
+      return error.response as AxiosResponse<R>;
+    }
+  }
+
+  protected async post<R = unknown>(data?: unknown): Promise<AxiosResponse<R>> {
+    try {
+      return await axios.post(this.urlBuilder(), data, { headers: this.headers });
+    } catch (error) {
+      if (!axios.isAxiosError(error)) {
+        throw new Error(UNEXPECTED_ERROR_OCCURRED);
+      }
+
+      return error.response as AxiosResponse<R>;
+    }
+  }
+
+  protected async put<R = unknown>(data?: unknown): Promise<AxiosResponse<R>> {
+    try {
+      return await axios.put(this.urlBuilder(), data, { headers: this.headers });
+    } catch (error) {
+      if (!axios.isAxiosError(error)) {
+        throw new Error(UNEXPECTED_ERROR_OCCURRED);
+      }
+
+      return error.response as AxiosResponse<R>;
+    }
+  }
+
+  protected async delete<R = unknown>(): Promise<AxiosResponse<R>> {
+    try {
+      return await axios.delete(this.urlBuilder(), { headers: this.headers });
+    } catch (error) {
+      if (!axios.isAxiosError(error)) {
+        throw new Error(UNEXPECTED_ERROR_OCCURRED);
+      }
+
+      return error.response as AxiosResponse<R>;
+    }
+  }
+
+  protected urlBuilder(): string {
+    return `${this.apiDomain}${this.endpoint}`;
+  }
+}
