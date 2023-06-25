7220bf55171f31514bc6199186bb277f14b49ef0
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-24 20:28:50 -0700

    test(topic):  add tests for topic-finder.service

---
+++ api/tests/unit/topic/topic-finder.service.test.ts
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { PrismaService } from '@/app/shared/persistance';
+import { PrimitiveTopic } from '@/app/topic/domain';
+import { TopicPostgresRepository } from '@/app/topic/persistance';
+import { TopicFinderService } from '@/app/topic/service';
+import { TopicFactory } from '@/utilities/factories/topic-factory';
+import { prismaMock } from '@/utilities/mocks';
+
+describe('topic-finder.service', () => {
+  let topicFinderService: TopicFinderService;
+
+  beforeEach(() => {
+    const topicPostgresRepository = new TopicPostgresRepository(
+      prismaMock as unknown as PrismaService
+    );
+    topicFinderService = new TopicFinderService(topicPostgresRepository);
+  });
+
+  describe('Find Many', () => {
+    it('Should return a list of topic', async () => {
+      const topics = [TopicFactory.create().toPrimitives(), TopicFactory.create().toPrimitives()];
+
+      prismaMock.topic.findMany.mockResolvedValue(topics);
+
+      const result = await topicFinderService.findMany();
+
+      expect(result[0]).toStrictEqual(topics[0]);
+      expect(result[1]).toStrictEqual(topics[1]);
+    });
+
+    it('Should return an empty list of topic', async () => {
+      const topics: PrimitiveTopic[] = [];
+
+      prismaMock.topic.findMany.mockResolvedValue(topics);
+
+      const result = await topicFinderService.findMany();
+
+      expect(result).toHaveLength(0);
+    });
+  });
+});
