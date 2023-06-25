668b6988c487c36ddc2a9b65342c535bd016f9ad
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-24 23:55:45 -0700

    feat(shared):  add HttpRepository

---
+++ web/src/core/shared/http-repository.ts
@@ -0,0 +1,53 @@
+export abstract class HttpRepository {
+  protected apiDomain = '';
+
+  protected endpoint = '';
+
+  protected headers: Headers = {};
+
+  /**
+   * Method to modify the current config of th request
+   * necessary when you want to modify some parameters like
+   * domain, endpoint or add headers before you execute the petition
+   *
+   * @example
+   * configRequest({
+   *   apiDomain: 'my-domain.com',
+   *   endpoint: '/order',
+   *   headers: {
+   *     'Content-Type': string
+   *   }
+   * })
+   */
+  protected abstract configRequest(config: ConfigRequest): void;
+
+  /**
+   * Method to add the token to the header of the request
+   */
+  protected abstract useToken(token: string): void;
+
+  /**
+   * Method to build the url with the current configuration of th request
+   * This method must be used inside of some of the petition methods (get, post, put or delete)
+   *
+   * @example
+   * const res = await fetch(urlBuilder(), config)
+   */
+  protected abstract urlBuilder(): string;
+
+  protected abstract get(): Promise<unknown>;
+
+  protected abstract post(data?: unknown): Promise<unknown>;
+
+  protected abstract put(data?: unknown): Promise<unknown>;
+
+  protected abstract delete(): Promise<unknown>;
+}
+
+export type ConfigRequest = {
+  endpoint: string;
+  apiDomain?: string;
+  headers?: Headers;
+};
+
+export type Headers = Record<string, string>;
